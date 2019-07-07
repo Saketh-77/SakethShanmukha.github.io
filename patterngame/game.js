942dddc8963a7fbb7e591eb7b26d04ba478b148e
SakethShanmukha <saisaketha01@gmail.com>
2019-07-07 16:35:18 +0530

    Creating a page to redirect to my websites

---
+++ patterngame/game.js
@@ -0,0 +1,63 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var randomChosenColor, gamePattern = [],
+  userClickedPattern = [];
+var level = 0, counter = 0;
+let gameOn = true;
+$(document).on("keypress", function () {
+  nextSequence();
+})
+
+function winCheck(currentLevel) {
+  if (gamePattern[currentLevel] !== userClickedPattern[currentLevel]) {
+    gameOn = false;
+    gameOver();
+  }
+  if (currentLevel + 1 === gamePattern.length && gameOn) {
+    setTimeout(function () {
+      nextSequence();
+    }, 1000);
+  }
+
+}
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  randomChosenColor = buttonColors[randomNumber];
+  level += 1;
+  $("#level-title").text("level " + level);
+
+  $("div#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
+
+  gamePattern.push(randomChosenColor);
+  userClickedPattern = [];
+}
+
+function playSound(name) {
+  var colorSound = new Audio("sounds/" + name + ".mp3");
+  colorSound.play();
+}
+
+function animatePress(currentColor) {
+  $("div#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("div#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+$("div.btn").on("click", function () {
+  var userChosenColor = $(this).attr("id");
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+  // console.log(userChosenColor);
+  userClickedPattern.push(userChosenColor);
+  var userIndex = userClickedPattern.length - 1;
+  // console.log(userClickedPattern);
+  winCheck(userIndex);
+
+})
+
+function gameOver() {
+  $("body").addClass("game-over");
+  $("#level-title").text("GAME OVER !!");
+}
